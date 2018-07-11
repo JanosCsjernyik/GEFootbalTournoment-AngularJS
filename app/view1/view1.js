c11ad3026e723e4d25611befb245a60f34e93950
János Csjernyik <janos.csjernyik@gmail.com>
2018-07-11 13:33:45 +0200

    demo

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -14,21 +14,69 @@ angular.module('myApp.view1', ['ngRoute'])
     "Kolumbia", "Anglia"];
 
     $scope.matchups = [];
+    var promise;
 
     playersService.getPlayers().get(function(playersJson){
-      var teams = [];
-      for(let name of $scope.teamNames){
+
+      $scope.enablePopover = function() {
+        $(function () {
+          $('[data-toggle="popover"]').popover('enable');
+        });
+      }
+      
+      $scope.disablePopover = function() {
+        $(function () {
+          $('[data-toggle="popover"]').popover('disable');
+        });
+      }
+
+      const teams = [];
+      for(let name of $scope.teamNames) {
         let randomTeam = { teamName:name , players:[], goalsShot: 0 };
         teams.push(randomTeam);
       }
-      fillTeamsWithPlayers(teams,playersJson);
-      $scope.teams = $scope.teamNames;
-      $scope.matchups.push(generatePairs(teams));
-      console.log("$scope.matchups: ", $scope.matchups, "len:", $scope.matchups.length);
-      $scope.teamPairs = $interval(generateMatchups(), 3000);
-    })
 
-    function fillTeamsWithPlayers(teams, playersJson){
+      $scope.fillTeamsWithPlayers(teams,playersJson);
+      $scope.teams = $scope.teamNames;
+      $scope.disableButton = false;
+      $scope.gameState = "stopped";
+      $scope.button = "Start Game";
+
+      $scope.controllGame = function() {
+        if ($scope.gameState === "stopped") {
+          if ($scope.matchups.length === 0) {
+            $scope.enablePopover();
+            $scope.matchups.push($scope.generatePairs(teams));
+          }
+          $scope.gameState = "run";
+          promise = $interval($scope.generateMatchups, 3000);
+          $scope.disablePopover();
+          $scope.button = "Stop Game";
+        } else { 
+          $scope.button = "Start Game";
+          $scope.gameState = "stopped";
+          $interval.cancel(promise);
+        }
+      }
+
+      $scope.resetGame = function() {
+        $scope.winner = null;
+        $scope.matchups.length = 0;
+
+        for(let name of $scope.teamNames){
+          let randomTeam = { teamName:name , players:[], goalsShot: 0 };
+          teams.push(randomTeam);
+        }
+        $scope.fillTeamsWithPlayers(teams,playersJson);
+
+        $scope.disableButton = false;
+        $scope.gameState = "stopped";
+        $scope.button = "Start Game";
+      }
+    });
+
+
+    $scope.fillTeamsWithPlayers = function(teams, playersJson) {
       var helperIndex = 0;
       for (let i = 0; i < teams.length; i++){
         for(let y = 0; y < 11; y++){
@@ -38,7 +86,7 @@ angular.module('myApp.view1', ['ngRoute'])
       }
     }
 
-    function generatePairs (teams) {
+    $scope.generatePairs = function(teams) {
       let arr1 = teams;
       let teamPairs = [];
       arr1.sort(function() { return 0.5 - Math.random();}); // shuffle arrays
@@ -50,52 +98,59 @@ angular.module('myApp.view1', ['ngRoute'])
             team1 : team1,
             team2 : team2
           };
-          team1.goalsShot = getRandomInt(0, 10);
-          team2.goalsShot = getRandomInt(0, 10);
-          while(team1.goalsShot == team2.goalsShot){
-              team1.goalsShot = getRandomInt(0, 10);
-          }
+          $scope.setGoals(team1, team2);
           teamPairs.push(addToPairs);
       }
-      console.log("teamPairs end of the function: ", teamPairs);
       return teamPairs;
   }
 
+  $scope.setGoals = function(team1, team2) {
+    team1.goalsShot = $scope.getRandomInt(0, 10);
+    team2.goalsShot = $scope.getRandomInt(0, 10);
+    while(team1.goalsShot === team2.goalsShot){
+        team1.goalsShot = $scope.getRandomInt(0, 10);
+    }
+  }
 
-  function pickWinnersFromMatches(match){
+
+  $scope.pickWinnersFromMatches = function(match){
     if(match.team1.goalsShot > match.team2.goalsShot){
       return match.team1;
     }
     return match.team2;
   }
 
-  function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  $scope.getRandomInt = function(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
   }
 
-  function generateMatchups() {
-    console.log('$scope.matchups: ', $scope.matchups, 'len: ', $scope.matchups.length);
-    if ($scope.matchups.length === 3){
-      let winner = pickWinnersFromMatches($scope.matchups[$scope.matchups.length - 1]);
-      $scope.matchups.push([winner]);
-      return $scope.matchups;
+  $scope.generateMatchups = function() {
+    if ($scope.matchups.length === 4){
+      $scope.winner = $scope.pickWinnersFromMatches($scope.matchups[3][0]).teamName;
+      $interval.cancel(promise);
+      $scope.disableButton = true;
+      return;
     }
+
     var arrayOfWinners = [];
     for (let teamPair of $scope.matchups[$scope.matchups.length - 1]){
-      let winner = pickWinnersFromMatches(teamPair);
+      let winner = Object.assign({}, $scope.pickWinnersFromMatches(teamPair));
       arrayOfWinners.push(winner);
     }
     var nextRound = [];
     while (arrayOfWinners.length) {
-      var team1 = arrayOfWinners.pop(),
-          team2 = arrayOfWinners.pop();
+      var team1 = arrayOfWinners.shift(),
+          team2 = arrayOfWinners.shift();
+          $scope.setGoals(team1,team2);
       let newMatch = {
         team1 : team1,
         team2 : team2
       };
       nextRound.push(newMatch);  
     }
-    $scope.matchups.push(nextRound)
-    return $scope.matchups;
+    $scope.matchups.push(nextRound)    
+    $scope.teamPairs = $scope.matchups;
   }
 }]);
